51577c9c214e47f9b670630d1c9b120036d335f9
Akira Hiro <metro@Akiras-MacBook-Pro.local>
2023-06-05 22:59:20 +0700

    Updated app.py with 3 predicitons and preprocessed 'uraian'.

--- app.py
+++ app.py
@@ -1,38 +1,73 @@
 from flask import Flask, request, jsonify
+import pandas as pd
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import re
 
-# Load the saved model
+# Load the trained model
 model = tf.keras.models.load_model('model.h5')
+model.summary()  # Optional: Print model summary to verify its structure
 
 # Create the Flask app
 app = Flask(__name__)
 
-# Create a TextVectorization layer and adapt it to the training data
-vectorizer = TextVectorization(max_tokens=10000, output_mode='count')
-vectorizer.adapt(X)  # Adapt with the same X used during training
-
+# Define a route to handle the API endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the input data from the request
-    data = request.json
-    uraian = data['uraian']
+    data = request.get_json()  # Get the JSON data from the request
+    uraian = data['uraian']  # Extract the 'uraian' input from the JSON data
 
     # Preprocess the input data
+    uraian = uraian.lower()
+    uraian = str(uraian)
+    uraian = re.sub(r'[^\w\s]+', ' ', uraian)
+    uraian = re.sub(r'_+', ' ', uraian)
+    uraian = re.sub('\s+', ' ', uraian, flags=re.UNICODE)
+    uraian = [uraian]
     uraian_counts = vectorizer(uraian).numpy()
 
-    # Convert the preprocessed data to TensorFlow tensor
-    input_data = tf.convert_to_tensor(uraian_counts, dtype=tf.float32)
 
-    # Make predictions using the loaded model
-    predictions = model.predict(input_data)
+    # Perform the prediction using the loaded model
+    predictions = model.predict(uraian_counts)
 
-    # Get the predicted labels
-    predicted_labels = [list(label_mapping.keys())[pred.argmax()] for pred in predictions]
+    # Get the top 3 predicted labels and their probabilities
+    top_labels = np.argsort(-predictions[0])[:3]  # Get the indices of the top 3 labels
+    predicted_labels = [label for label, index in label_mapping.items() if index in top_labels]
+    label_probabilities = predictions[0][top_labels]
 
-    # Return the predicted labels as the API response
-    response = {'topik': predicted_labels}
-    return jsonify(response)
+    # Create a JSON response with the predicted labels and probabilities
+    response = {'   ': []}
+    for label, probability in zip(predicted_labels, label_probabilities):
+        response['predictions'].append({'label': label, 'probability': float(probability)})
+
+    return jsonify(response)  # Return the JSON response
+  # Return the JSON response
 
 if __name__ == '__main__':
+    # Load the dataset .csv with ',' separator
+    data = pd.read_csv('deskripsi_permasalahan.csv', sep=',', low_memory=False)
+    df = pd.DataFrame(data)
+
+    # Data cleaning
+    input_column = 'Uraian'
+    output_column = 'Topik'
+    df[input_column] = df[input_column].map(str)
+    df[input_column] = df[input_column].str.lower()
+    df[input_column] = df[input_column].str.replace(r'[^\w\s]+', ' ')
+    df[input_column] = df[input_column].str.replace(r'_+', ' ')
+    df[input_column] = df[input_column].str.replace('\s+', ' ', regex=True)
+
+    # Dividing data into input and output
+    x = df.loc[:, input_column]
+    y = df.loc[:, output_column]
+    print('Data Divided Successfully Into Input & Output')
+
+    # Create a TextVectorization layer
+    vectorizer = TextVectorization(max_tokens=1904, output_mode='count')
+    vectorizer.adapt(x)
+
+    # Convert labels to numerical values
+    label_mapping = {label: index for index, label in enumerate(set(y))}
+    
     app.run()
